3f6206cc243d4a83a5f5d5450d8b03905e91efd5
Kenneth Lee <kennethkl@gmail.com>
2013-04-12 23:40:27 -0400

    added Login To Facebook

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,26 @@
-console.log('whoa')
+(function(namespace) {
+
+  // Models
+
+  // Controls
+  var Login = can.Control({
+    init: function() {
+      console.log(this);
+      this.element.html(can.view('/static/views/login.ejs'));
+    },
+    
+    '#loginToFacebook click': function(el, event) {
+      FB.login();
+    }
+  });
+
+
+  // Start
+  $(function() {
+    namespace.login = new Login('#login');
+    
+  });
+
+  namespace.Login = Login;
+
+})(window);
