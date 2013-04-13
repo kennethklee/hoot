8503d44f88be55a06dc3a71867bc48d6654a2fa7
Kenneth Lee <kennethkl@gmail.com>
2013-04-13 00:52:21 -0400

    added some facebook api login stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,40 @@
 (function(namespace) {
 
   // Models
+  var User = can.Model({},{});
 
   // Controls
   var Login = can.Control({
     init: function() {
-      console.log(this);
       this.element.html(can.view('/static/views/login.ejs'));
     },
+
+    onConnected: function() {
+      var self = this;
+      console.log('logged in');
+      FB.api('/me', function(response) {
+        //self.options.user = User.model(response);
+        new Home('#home', {user: response});
+        $('#app').attr('page', 'home');
+      });
+    },
     
-    '#loginToFacebook click': function(el, event) {
-      FB.login();
+    '.login click': function(el, event) {
+      var self = this;
+      FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+          self.onConnected();
+        } else {
+          FB.login(function() {self.onConnected()});
+        }
+      });
+    },
+  });
+
+  var Home = can.Control({
+    init: function() {
+      console.log(this.options);
+      this.element.html(can.view('/static/views/home.ejs', this.options));
     }
   });
 
@@ -18,9 +42,9 @@
   // Start
   $(function() {
     namespace.login = new Login('#login');
-    
   });
 
   namespace.Login = Login;
+  namespace.Home = Home;
 
 })(window);
